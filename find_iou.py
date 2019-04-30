94622a3a1070247cc782109cbafcc5e4df61dc2f
Shuijing725 <minnie725@outlook.com>
2019-04-29 21:58:46 -0500

    iou file fixed'

--- find_iou.py
+++ find_iou.py
@@ -7,43 +7,64 @@ fcn_folder = '/home/shuijing/Desktop/ece498sm_project/runs/1556505662.8736737'
 knn_folder = '/home/shuijing/Desktop/ece498sm_project/knn_train'
 gt_folder = '/home/shuijing/Desktop/ece498sm_project/data_road/training/gt_image_2'
 
+def is_gray(pixel):
+    if pixel[0] == 128 and pixel[1] == 127 and pixel[2] == 128:
+        return True
+    else:
+        return False
+
+def is_pink(pixel):
+    if pixel[0] == 255 and pixel[1] == 0 and pixel[2] == 255:
+        return True
+    else:
+        return False
+
 # gt_folder: the ground truth labels folder (/data_road/training/gt_image_2)
 # folder: the folder containing your results
 # If you are running it on a single image, just get rid of the outer-most for loop
-def calc_iou(gt_folder, folder):
-    k = 0
-    iou = defaultdict(float)
+def calc_iou(gt_folder, folder, method = 'knn'):
+    iou = 0.0
+    count = 0
 
-    print('reading ground truth')
-    # read the ground truth
-    for file in os.listdir(gt_folder):
-        k += 1
-        img = cv2.imread(os.path.join(gt_folder, file))
-        height, width, _ = img.shape
-        # count the tot number of pink pixels
-        for i in range(height):
-            for j in range(width):
-                if img[i, j, 0] == 255 and img[i, j, 1] == 0 and img[i, j, 2] == 255:
-                    iou[k] += 1.0
-
-    print('reading predictions')
-    k = 0
-    pred = defaultdict(float)
     for file in os.listdir(folder):
-        k += 1
+        # print(file)
+
+        # find the corresponding ground truth in gt_folder
+        if method == 'knn':
+            if file.startswith('um_'):
+                gt_name = 'um_road_' + str(file[-10:])
+            elif file.startswith('umm_'):
+                gt_name = 'umm_road_' + str(file[-10:])
+            else:
+                gt_name = 'uu_road_' + str(file[-10:])
+
+        # fcn
+        else:
+            gt_name = file
+        # load predicted img and gt_img
         img = cv2.imread(os.path.join(folder, file))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        gt = cv2.imread(os.path.join(gt_folder, gt_name))
+        gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+
         height, width, _ = img.shape
+        gt = cv2.resize(gt, (width, height))
+        intersect = 0.0
+        union = 0.0
+
         # count tot number of gray pixels
         for i in range(height):
             for j in range(width):
-                if img[i, j, 0] == 128 and img[i, j, 1] == 127 and img[i, j, 2] == 128:
-                    pred[k] += 1.0
+                # if intersection
+                if is_gray(img[i, j]) and is_pink(gt[i, j]):
+                    intersect += 1
+                if is_gray(img[i, j]) or is_pink(gt[i, j]):
+                    union += 1
 
-    print('calculating iou')
-    tot = 0.0
-    for key in iou:
-        tot += pred[key] / iou[key]
+        if union != 0:
+            count += 1
+            iou += intersect / union
 
-    print('IoU = ', tot / len(iou))
+    print('IoU = ', iou / count)
 
-calc_iou(gt_folder, knn_folder)
+calc_iou(gt_folder, fcn_folder, method = 'fcn')
